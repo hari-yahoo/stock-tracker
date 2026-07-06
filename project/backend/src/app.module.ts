23113c268ddb6ba8c7b65c4e42aa2f197fc75011
hari-yahoo <hgokulam@yahoo.com>
2026-07-06 23:28:05 +0530

    wiring, removed dist folder

--- project/backend/src/app.module.ts
+++ project/backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // backend/dist/../public → backend/public
+      exclude: ['/api/{*splat}'],
+    }),
+    // ...your feature modules
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
